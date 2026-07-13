33c4ee7a4fb37efda7f06a247fdc4af9b4f115d0
Mark Devenyi <markdevenyidev@gmail.com>
2026-07-13 16:40:50 -0400

    feat(isa/x86): add immediate encoder

---
+++ src/x86/encode/imm.c
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <stddef.h>
+
+#include "x86/instr.h"
+
+void encode_imm(instr_t* instruction) {
+    const instrform_t* form = instruction->form;
+    const operand_t* imm_op = NULL;
+    switch (form->form) {
+        case MI:
+            imm_op = &instruction->operands[1];
+            break;
+        case I:
+            imm_op = &instruction->operands[1];
+            break;
+        default:
+            return;
+    }
+
+    size_t bytes = form->s1 / 8;
+    assert(bytes < 9 && "Immediate length exceeds 8 bytes!");
+
+    __builtin_memcpy(
+        instruction->binary + instruction->binary_index,
+        &imm_op->immediate,
+        bytes
+    );
+    instruction->binary_index += 8;
+}
