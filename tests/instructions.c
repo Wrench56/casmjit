de807cf0609f27e88104b9a0a6c505b08e546667
Mark Devenyi <markdevenyidev@gmail.com>
2026-07-14 14:58:23 -0400

    test(isa/x86): add instruction tests

---
+++ tests/instructions.c
@@ -0,0 +1,105 @@
+#include "kritic.h"
+
+#include <stdio.h>
+
+#include "x86/instr.h"
+
+static __attribute__((unused)) void debug(const instr_t* instr) {
+    printf("Debug = ");
+    for (size_t i = 0; i < instr->binary_index; i++) {
+        printf("%02X", instr->binary[i]);
+    }
+    printf("\n");
+    printf("Index = %u\n", instr->binary_index);
+}
+
+const operand_t rr_ops[4] = {
+        {
+            .kind = OPK_REG,
+            .reg = REG_RAX,
+        },
+        {
+            .kind = OPK_REG,
+            .reg = REG_RBX,
+        }
+    };
+
+const operand_t ri32_ops[4] = {
+        {
+            .kind = OPK_REG,
+            .reg = REG_RAX,
+        },
+        {
+            .kind = OPK_IMM,
+            .immediate = 0xAF,
+        }
+    };
+
+const operand_t rm_ops[4] = {
+        {
+            .kind = OPK_REG,
+            .reg = REG_RAX,
+        },
+        {
+            .kind = OPK_MEM,
+            .base = REG_RBX,
+            .index = REG_INVD,
+            .scale = 1,
+        },
+    };
+
+KRITIC_TEST(instructions, add) {
+    instr_t instr = { 0 };
+
+    const uint8_t out_rr[] = {
+        0x48, 0x01, 0xD8, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    };
+
+    const uint8_t out_rm[] = {
+        0x48, 0x03, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    };
+
+    const uint8_t out_ri32[] = {
+        0x48, 0x81, 0xC0, 0xAF, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    };
+
+    to_instr(ADD, rr_ops, &instr);
+    KRITIC_ASSERT_EQ(0, memcmp(out_rr, instr.binary, instr.binary_index));
+
+    to_instr(ADD, rm_ops, &instr);
+    KRITIC_ASSERT_EQ(0, memcmp(out_rm, instr.binary, instr.binary_index));
+
+    to_instr(ADD, ri32_ops, &instr);
+    KRITIC_ASSERT_EQ(0, memcmp(out_ri32, instr.binary, instr.binary_index));
+}
+
+KRITIC_TEST(encode, mov) {
+    instr_t instr = { 0 };
+
+    const uint8_t out_rr[] = {
+        0x48, 0x89, 0xD8, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    };
+
+    const uint8_t out_rm[] = {
+        0x48, 0x8B, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    };
+
+    const uint8_t out_ri32[] = {
+        0x48, 0xC7, 0xC0, 0xAF, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    };
+
+    to_instr(MOV, rr_ops, &instr);
+    KRITIC_ASSERT_EQ(0, memcmp(out_rr, instr.binary, instr.binary_index));
+
+    to_instr(MOV, rm_ops, &instr);
+    KRITIC_ASSERT_EQ(0, memcmp(out_rm, instr.binary, instr.binary_index));
+
+    to_instr(MOV, ri32_ops, &instr);
+    KRITIC_ASSERT_EQ(0, memcmp(out_ri32, instr.binary, instr.binary_index));
+}
